82a0c32be948f5d41eeb039cefbb5fa9f46f24fd
Rodrigo Gama <rodrigo1902.rb@gmail.com>
2023-09-28 19:44:49 -0300

    Implemented corner and climb areas

--- python/common/pathfinder.py
+++ python/common/pathfinder.py
@@ -11,6 +11,7 @@ class Tile(Enum):
     PATHTARGET = 'XX'
     PATHFLOOR = '\u2593\u2593'
     PATHWALL = '\u2591\u2591'
+    PATHCORNER = '\u2592\u2592'
     PATHUP = 2
     PATHDOWN = 3
     PATHLEFT = 4
@@ -65,6 +66,24 @@ class Grid:
     def _is_in_bounds(self, x, y):
         return x >= 0 and y >= 0 and x < len(self._tiles) and y < len(self._tiles[0])
     
+    def get_tile_left(self, x, y):
+        new_x = x - 1
+        if not self._is_in_bounds(new_x, y):
+            return None
+        return self._tiles[new_x][y]
+    
+    def get_tile_right(self, x, y):
+        new_x = x + 1
+        if not self._is_in_bounds(new_x, y):
+            return None
+        return self._tiles[new_x][y]
+    
+    def get_tile_below(self, x, y):
+        new_y = y - 1
+        if not self._is_in_bounds(x, new_y):
+            return None
+        return self._tiles[x][y-1]
+    
     def get_tile_above(self, x, y):
         new_y = y + 1
         if not self._is_in_bounds(x, new_y):
@@ -105,17 +124,47 @@ class PathFinder:
         for i in range(len(self._grid._tiles)):
             for j in range(len(self._grid._tiles[0])):
                 if self._grid._tiles[i][j] == Tile.WALL:
-                    tile_above = self._grid.get_tile_above(i, j)
-                    if tile_above == Tile.EMPTY:
+                    tile_left = self._grid.get_tile_above(i, j)
+                    if tile_left == Tile.EMPTY:
                         self._grid.set_tile(i, j, Tile.PATHFLOOR)
 
+    def _generate_wall_climb_areas(self):
+
+        for i in range(len(self._grid._tiles)):
+            for j in range(len(self._grid._tiles[0])):
+                if self._grid._tiles[i][j] == Tile.WALL:
+                    tile_left = self._grid.get_tile_left(i, j)
+                    if tile_left == Tile.EMPTY:
+                        self._grid.set_tile(i, j, Tile.PATHWALL)
+                        continue
+                    tile_right = self._grid.get_tile_right(i, j)
+                    if tile_right == Tile.EMPTY:
+                        self._grid.set_tile(i, j, Tile.PATHWALL)
+                        continue
+
+    def _generate_corner_climb_areas(self):
+
+        for i in range(len(self._grid._tiles)):
+            for j in range(len(self._grid._tiles[0])):
+                if self._grid._tiles[i][j] == Tile.PATHFLOOR:
+                    tile_left = self._grid.get_tile_left(i, j)
+                    if tile_left == Tile.EMPTY:
+                        self._grid.set_tile(i, j, Tile.PATHCORNER)
+                        continue
+                    tile_right = self._grid.get_tile_right(i, j)
+                    if tile_right == Tile.EMPTY:
+                        self._grid.set_tile(i, j, Tile.PATHCORNER)
+                        continue
+
+                    
     def update_grid(self, grid_data):
         self._grid = Grid(grid_data)
+
         self._generate_floor_areas()
+        self._generate_corner_climb_areas()
+        self._generate_wall_climb_areas()
 
     def display(self):
-
-        print(self._grid)
 
         if not self.last_displayed_grid or (not self._grid == self.last_displayed_grid):
             print(self._grid)
